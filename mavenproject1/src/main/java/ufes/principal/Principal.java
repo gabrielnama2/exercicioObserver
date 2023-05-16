f16bc12ba7151c9e945996aecff46076df49ff1b
Gabriel Namã <gabrielnama2@gmail.com>
2023-05-15 23:20:42 -0300

    V(1.0)
    Primeiro commit.

--- mavenproject1/src/main/java/ufes/principal/Principal.java
+++ mavenproject1/src/main/java/ufes/principal/Principal.java
@@ -17,5 +17,5 @@ public class Principal {
         estacaoClimatica.atualizarMedicoes(25.5f, 65f, 1013.1f, LocalDate.of(2023, 5, 15));
         estacaoClimatica.atualizarMedicoes(26.0f, 68f, 1012.5f, LocalDate.of(2023, 5, 16));
         estacaoClimatica.atualizarMedicoes(24.0f, 60f, 1015.5f, LocalDate.of(2023, 5, 17));
-    } 
+    }
 }

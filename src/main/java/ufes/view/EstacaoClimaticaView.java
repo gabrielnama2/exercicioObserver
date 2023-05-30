05369335f601754d95ed0c23740e43ec66f6f51d
Gabriel Namã <gabrielnama2@gmail.com>
2023-05-30 18:01:35 -0300

    Revert "Telas centralizadas"
    This reverts commit 57ba9b68e04da78963c167ab0bf3ef677d7d6a71.

--- src/main/java/ufes/view/EstacaoClimaticaView.java
+++ src/main/java/ufes/view/EstacaoClimaticaView.java
@@ -1,7 +1,6 @@
 package ufes.view;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GraphicsEnvironment;
 import static java.awt.image.ImageObserver.WIDTH;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
@@ -15,7 +14,6 @@ public class EstacaoClimaticaView extends javax.swing.JFrame {
     
     public EstacaoClimaticaView() {
         initComponents();
-        centralizarTela();
         //this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setResizable(false);
         this.setVisible(true);  
@@ -58,21 +56,6 @@ public class EstacaoClimaticaView extends javax.swing.JFrame {
     public void exibirMensagemExcecao(String mensagem) {
         JOptionPane.showMessageDialog(null, "Erro: " + mensagem, "Mensagem de erro", WIDTH);
         this.setVisible(true);
-    }
-    
-    private void centralizarTela(){
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        
-        //Obtém a posição central da tela
-        int screenWidth = ge.getCenterPoint().x;
-        int screenHeight = ge.getCenterPoint().y;
-        
-        //Calcula a posição central da janela
-        int janelaX = screenWidth - (this.getWidth()/2);
-        int janelaY = screenHeight - (this.getHeight()/2);
-        
-        //Define a posição na tela
-        setLocation(janelaX, janelaY);
     }
     
     @SuppressWarnings("unchecked")
@@ -122,19 +105,19 @@ public class EstacaoClimaticaView extends javax.swing.JFrame {
         jLabelTemperatura.setText("Temperatura:");
 
         txtResultadoTemperatura.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        txtResultadoTemperatura.setText("0");
+        txtResultadoTemperatura.setText("TEMPERATURA");
 
         jLabelUmidade.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabelUmidade.setText("Umidade:");
 
         txtResultadoUmidade.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        txtResultadoUmidade.setText("0");
+        txtResultadoUmidade.setText("UMIDADE");
 
         jLabelPressao.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabelPressao.setText("Pressão:");
 
         txtResultadoPressao.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        txtResultadoPressao.setText("0");
+        txtResultadoPressao.setText("PRESSÃO");
 
         jLabelData.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabelData.setText("Data:");
@@ -207,25 +190,25 @@ public class EstacaoClimaticaView extends javax.swing.JFrame {
         jLabelTemperaturaMedia.setText("Temperatura:");
 
         txtResultadoTemperaturaMedia.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        txtResultadoTemperaturaMedia.setText("0");
+        txtResultadoTemperaturaMedia.setText("TEMPERATURA");
 
         jLabelUmidadeMedia.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabelUmidadeMedia.setText("Umidade:");
 
         txtResultadoUmidadeMedia.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        txtResultadoUmidadeMedia.setText("0");
+        txtResultadoUmidadeMedia.setText("UMIDADE");
 
         jLabelPressaoMedia.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabelPressaoMedia.setText("Pressão:");
 
         txtResultadoPressaoMedia.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        txtResultadoPressaoMedia.setText("0");
+        txtResultadoPressaoMedia.setText("PRESSÃO");
 
         jLabelNumRegistros.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabelNumRegistros.setText("Nº de registros:");
 
         txtResultadoNumRegistros.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        txtResultadoNumRegistros.setText("0");
+        txtResultadoNumRegistros.setText("REGISTROS");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -311,7 +294,7 @@ public class EstacaoClimaticaView extends javax.swing.JFrame {
 
         txtqtdRegistros.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         txtqtdRegistros.setForeground(new java.awt.Color(255, 255, 255));
-        txtqtdRegistros.setText("0");
+        txtqtdRegistros.setText("99999");
 
         jLabelqtdRegistros.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabelqtdRegistros.setForeground(new java.awt.Color(255, 255, 255));

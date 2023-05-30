0038acad427658d0265579f1cddaec3a1594e9ca
Gabriel Namã <gabrielnama2@gmail.com>
2023-05-30 18:01:14 -0300

    Revert "DAO criada"
    This reverts commit fd5a9a088f99a160daa93a1824b139b200ab4863.

--- src/main/java/dao/DAO.java
+++
@@ -1,7 +0,0 @@
-package dao;
-import ufes.model.DadoClima;
-
-public interface DAO {
-    public void incluir(DadoClima dadoClima);
-    public void remover(DadoClima dadoClima);
-}
---
+++ src/main/java/dao/SalvaDadosClimaArquivo.java
@@ -0,0 +1,5 @@
+package dao;
+
+public class SalvaDadosClimaArquivo {
+    private String caminhoArquivo = "";
+}
